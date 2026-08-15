dba7d7d7da237d67d57c7f2c7abba276a8586319
Qing <2363806164@qq.com>
2026-08-15 22:16:38 +0800

    Fix CSS syntax and mobile layout

--- newpost.sh
+++ newpost.sh
@@ -26,6 +26,7 @@ cat > "$FILENAME" <<EOF
 <html>
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>主标题</title>
 	<script src="/js/highlight.min.js"></script>
 	<link rel="stylesheet" href="/css/link.css">
