e65cb610456d5e68a323a486318b36e27ccc9f25
Qing <2363806164@qq.com>
2026-08-19 22:11:07 +0800

    Fix mobile footer license alignment

--- js/footer.js
+++ js/footer.js
@@ -6,7 +6,7 @@
                 <span>Powered by <a href="https://nginx.org/" target="_blank" rel="noopener">Nginx</a> &amp; 古法 HTML/CSS</span>
                 <span class="cc-logo">
                     <a href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank" rel="license noopener">
-                        <img src="https://licensebuttons.net/l/by-nc/4.0/80x15.png" alt="Creative Commons License" style="height: 1em; display: inline-block; vertical-align: middle; padding-bottom: 3px;">
+                        <img src="https://licensebuttons.net/l/by-nc/4.0/80x15.png" alt="Creative Commons License">
                         CC BY-NC
                     </a>
                 </span>
